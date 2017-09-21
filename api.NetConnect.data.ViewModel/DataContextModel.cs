50bc45946439fc76b81abe8b3cf787dc498e9010
Joachim-PC\Joachim <joachimgotzes@gmail.com>
2017-09-21 14:57:46 +0200

    no message

---
+++ api.NetConnect.data.ViewModel/DataContextModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using api.NetConnect.data.ViewModel.Profile;
+namespace api.NetConnect.data.ViewModel
+{
+    public class DataContextModel
+    {
+        public ProfileViewModel User { get; set; }
+    }
+}
