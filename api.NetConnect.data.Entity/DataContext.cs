225c26a552c9a21df3235936d63a02b8edafeea6
Joachim-PC\Joachim <joachimgotzes@gmail.com>
2017-09-27 17:18:07 +0200

    no message

--- api.NetConnect.data.Entity/DataContext.cs
+++ api.NetConnect.data.Entity/DataContext.cs
@@ -21,17 +21,7 @@ namespace api.NetConnect.data.Entity
             :base(ConnectionString)
         {
 
-        }
-        public override int SaveChanges()
-        {
-            DateTime saveTime = DateTime.UtcNow;
-            foreach (var entry in this.ChangeTracker.Entries().Where(e => e.State == (EntityState)System.Data.Entity.EntityState.Added || e.State == (EntityState)System.Data.Entity.EntityState.Modified))
-            {
-                if (entry.Property("LastChange").CurrentValue == null)
-                    entry.Property("LastChange").CurrentValue = BitConverter.GetBytes(DateTime.MinValue.Ticks);
-            }
-            return base.SaveChanges();
-        }
+        }        
     }
     
 }
