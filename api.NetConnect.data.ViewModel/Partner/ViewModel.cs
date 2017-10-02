9a55c4f71ca35c655395e53ac27ea8dc1668364a
Joachim-PC\Joachim <joachimgotzes@gmail.com>
2017-10-02 03:10:19 +0200

    PartnerViewModel

---
+++ api.NetConnect.data.ViewModel/Partner/ViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api.NetConnect.data.ViewModel.Partner
+{
+    class PartnerViewModel : BaseViewModel
+    {
+        public PartnerViewModelItem Data { get; set; }
+        public PartnerViewModel()
+        {
+            Data = new PartnerViewModelItem();
+        }
+    }
+    public class PartnerViewModelItem : BaseViewModelItem
+    {
+
+    }
+}
