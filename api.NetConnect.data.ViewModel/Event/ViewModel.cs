6ee37d2ef0c5680222bcdb40030233fe537a2fc2
Marius Hartmann <marius8hartmann@gmail.com>
2017-10-17 10:39:01 +0200

    Event ViewModel

--- api.NetConnect.data.ViewModel/Event/ViewModel.cs
+++ api.NetConnect.data.ViewModel/Event/ViewModel.cs
@@ -18,42 +18,14 @@ namespace api.NetConnect.data.ViewModel.Event
     public class EventViewModelItem : BaseViewModelItem
     {
         public String Title { get; set; }
-        public DateTime Date { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
         public String Image { get; set; }
         public String Text { get; set; }
-        public String Link { get; set; }
-        public List<FbLike> Likes { get; set; }
-        public List<FbComment> Comments { get; set; }
 
         public EventViewModelItem()
         {
-            Likes = new List<FbLike>();
-            Comments = new List<FbComment>();
+
         }
-    }
-
-    public class FbLike
-    {
-        public String Name { get; set; }
-        public String Image { get; set; }
-        public String Link { get; set; }
-    }
-
-    public class FbComment : FbBaseComment
-    {
-        public List<FbBaseComment> Comments { get; set; }
-
-        public FbComment()
-        {
-            Comments = new List<FbBaseComment>();
-        }
-    }
-
-    public class FbBaseComment
-    {
-        public String Name { get; set; }
-        public String Link { get; set; }
-        public DateTime Date { get; set; }
-        public String Text { get; set; }
     }
 }
